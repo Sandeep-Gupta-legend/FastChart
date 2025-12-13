036bdf104b77aabfb339e8f5e974ad82a3d2a8c2
Sandeep-Gupta-legend <sandeep1legend@gmail.com>
2025-12-13 11:39:55 +0530

    Initial commit

---
+++ server/models/Message.js
@@ -0,0 +1,34 @@
+import mongoose  from "mongoose";
+
+
+
+import { type } from "os";
+const messageSchema = new mongoose.Schema({
+    senderId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",required:true
+    },
+
+     receiverId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",required:true
+    },
+
+    text:{
+        type:String,
+        
+    },
+    image:{
+        type:String,
+        
+    },
+    seen:{
+        type:Boolean,
+        default:false
+    }
+  
+},{timestamps:true})
+
+const message=mongoose.model("Message",messageSchema)
+
+export default message;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import "dotenv/config";
+import http from 'http';
+
+import {connectDB} from './lib/db.js'
+import userRouter from './routes/userRoutes.js';
+
+const app=express();
+const server = http.createServer(app);
+
+app.use(express.json({limit:"4mb"}));
+app.use(cors());
+app.use("/api/status",(req,res)=>res.send("server is live"))
+app.use("/api/auth",userRouter);
+
+await connectDB();
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=> console.log("Server is running on "+PORT))
